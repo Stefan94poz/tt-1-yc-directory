744360e25907a2bbfccf2eef2caa0e50ff531338
stefaneze <stefan.s.webdev@gmail.com>
2025-01-03 20:28:24 +0100

    adding github login and navbar

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { auth, signIn, signOut } from "@/auth";
+
+const Navbar = async () => {
+  const session = await auth();
+  return (
+    <div className="px-5 py-3 bg-slate-900 shadow-sm font-work-sans">
+      <nav className="flex justify-between items-center">
+        <Link href="/">
+          <Image src="/next.svg" alt="vercel" width={100} height={24} />
+        </Link>
+        <div className="flex items-center gap-5">
+          {session && session?.user ? (
+            <>
+              <Link href="/startup/create">Create</Link>
+              {/* <button onClick={() => signOut()}>signOut</button> */}
+              <form
+                action={async () => {
+                  "use server";
+
+                  await signOut();
+                }}
+              >
+                <button type="submit">signOut</button>
+              </form>
+              <Link href={`/user/${session.user.id}`}>
+                <span>{session?.user?.name}</span>
+              </Link>
+            </>
+          ) : (
+            <form
+              action={async () => {
+                "use server";
+
+                await signIn("github");
+              }}
+            >
+              <button type="submit">Login</button>
+            </form>
+          )}
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
