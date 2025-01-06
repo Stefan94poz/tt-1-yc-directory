119405e360b4ed3421e5b312b50e93150840c46b
stefaneze <stefan.s.webdev@gmail.com>
2025-01-06 22:04:36 +0100

    Adding payload and supabase db, looks cool

--- app/(app)/(root)/page.tsx
+++ app/(app)/(root)/page.tsx
@@ -1,5 +1,5 @@
 import StartupCard from "@/components/StartupCard";
-import SearchForm from "../../components/SearchForm";
+import SearchForm from "../../../components/SearchForm";
 
 export default async function Home({
   searchParams,
